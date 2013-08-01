3f0f47bcd044be79af378ba66cb8836a453ee67a
Kody Dahl <kody@whiteboard.is>
2013-08-01 17:19:00 -0400

    Change worldvision logo

--- js/script.js
+++ js/script.js
@@ -6,15 +6,17 @@
 
 }(jQuery, window));
 
-			
+$('.more').on('click',function(event){
+	event.preventDefault();
+	var desc = $(this).attr('data-description');
+	//var speaker = $(this).attr('data-title');
 
-/*Quote slider*/
+	$('.box').find('.description').html(desc);
 
+	$('.box').addClass('.visible');
+});
 
-
-
-
-
-
-
-
+4('.close')on('click',function(event){
+	event.preventDefault();
+	$('.box').removeClass('.visible');
+});
